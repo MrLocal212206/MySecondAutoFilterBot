2bf44aae6bf462b94055712dbe9e8fed62ae061f
Loveissacrifice <87993223+Loveissacrifice@users.noreply.github.com>
2021-07-27 10:11:38 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,14 +28,14 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = f"{file_name} \n ⚜️Best Regards⚜️",
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '🔰Our Group🔰', url="https://t.me/joinchat/w_4P9qzo7tQzMTZl"
                                 )
                         ]
                     ]
